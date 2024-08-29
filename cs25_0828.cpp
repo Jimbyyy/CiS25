82aa720ba2de67dd2fddff47c6e613ca62d485e0
Jimmy Hsu <jimmydenghsu@gmail.com>
2024-08-28 17:53:30 -0700

    Added new source file from 08-28 course assignment

---
+++ cs25_0828.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Initializing variables
+	int num1 = 1;
+	int num2 = 2;
+	cout << "num1: " << num1
+		 << "\nnum2: " << num2;
+
+	// Swapping variables
+	num1 = 2;
+	num2 = 1;
+	cout << "\n\nSwapped: "
+		<< "\nnum1: " << num1
+		<< "\nnum2: " << num2;
+}
